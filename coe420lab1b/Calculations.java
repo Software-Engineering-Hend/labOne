663211408a10f5407107a300bff5b62d670d3dec
Hima Bijulal <g00065495@aus.edu>
2020-02-04 14:22:17 +0400

    Division implemented

--- coe420lab1b/Calculations.java
+++ coe420lab1b/Calculations.java
@@ -14,7 +14,7 @@ public class Calculations {
 		//member_B add the feature of subtracting x from y and print the result
 		
 		//member_C add the feature of dividing x by y and print the result
-		
+		int quotient = x/y;
 		//member_D add the feature of multiplying x by y and print the result
 		
 		
